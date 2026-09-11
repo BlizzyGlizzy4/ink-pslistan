2f159820898cec94b0fe23987ca51f547b7d67be
BlizzyGlizzy4 <andreas.ackovski@gmail.com>
2026-09-11 14:56:52 +0200

    Läser input och avgör om det är ett tal eller text.

--- program.cs
+++ program.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 List<string> names = new List<string>();
 List<int> prices = new List<int>();
 
@@ -10,11 +12,23 @@ prices.Add(32);
 names.Add("Ost");
 prices.Add(89);
 
-int total = 0; 
-for (int i = 0; i < names.Count; i++)
+while (true)
 {
-    Console.WriteLine($"{i +1}. {names[i]} - {prices[i]} kr");
-    total += prices[i];
-}
+    int total = 0;
+    for (int i = 0; i < names.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {names[i]} - {prices[i]} kr");
+        total = total + prices[i];
+    }
+    Console.WriteLine($"Totalt: {total} kr");
 
-Console.WriteLine($"Totalt: {total} kr");
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out int position))
+    {
+        Console.WriteLine($"Du vill ta bort vara nummer {position}");
+    }
+    else
+    {
+        Console.WriteLine($"Du vill lägga till en ny vara: {input}");
+    }
+}
